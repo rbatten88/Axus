e4a37f37c0e36c3ffdfb50b06be86789bf450bb6
rbatten88 <rbattenjr@gmail.com>
2021-01-19 21:40:03 -0500

    created products class views

---
+++ products/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('add/', views.ProductCreateView.as_view(), name='product_add'),
+	path('list/', views.ProductListView.as_view(), name="product_list"),
+	path('detail/<int:pk>', views.ProductUpdateView.as_view(), name="product_detail"),
+	path('delete/<int:pk>', views.ProductDeleteView.as_view(), name="product_delete"),
+	path('upload/', views.product_upload, name="customer_upload")
+]
+ 
--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 
 # Create your views here.
+ class ProductCreateView(CreateView):
+ 	pass
+
+
+ class ProductListView(ListView):
+ 	pass
+
+
+ class ProductUpdateView(UpdateView):
+ 	pass
+
+
+ class ProductDeleteView(DeleteView):
+ 	pass
+ 	
